9a98c7058428d88b73f9e8412d700d4b48b46541
yuyang <yuyang545262477@gmail.com>
2022-09-26 22:08:37 +0800

    app: after uploaded sync to firebase storage

--- components/main/Save.tsx
+++ components/main/Save.tsx
@@ -1,6 +1,4 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from 'firebase/compat';
 import 'firebase/compat/storage';
 
 import React, { useState } from 'react';
@@ -10,22 +8,41 @@ const Save = (props) => {
     const [caption, setCaption] = useState('');
     const image = props.route.params.image;
     console.log('urI:', image);
+
+    const savePostData = (downloadUrl: string) => {
+        firebase
+            .firestore()
+            .collection('posts')
+            .doc(firebase.auth().currentUser.uid)
+            .collection('userPosts')
+            .add({
+                downloadUrl,
+                caption,
+                creation: firebase.firestore.FieldValue.serverTimestamp()
+            })
+            .then(() => {
+                props.navigation.popToTop();
+            });
+    };
+
     const uploadVideo = async () => {
         try {
             const uri = props.route.params.image;
             const response = await fetch(uri);
             const blob = await response.blob();
-            // @ts-ignore
+
             const childPath = `post/${
                 firebase.auth().currentUser.uid
             }/${Math.random().toString(36)}`;
-            // @ts-ignore
+
             const task = firebase.storage().ref().child(childPath).put(blob);
+
             const taskProgress = (snapshot) => {
                 console.log(`transferred: ${snapshot.bytesTransferred}`);
             };
             const taskCompleted = () => {
                 task.snapshot.ref.getDownloadURL().then((snapshot) => {
+                    savePostData(snapshot);
                     console.log(snapshot);
                 });
             };
